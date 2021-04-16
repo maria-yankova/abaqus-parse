1c9e15d88d775f807af05d19c9ca3a533550523e
APlowman <adam.jp@live.co.uk>
2021-04-16 10:41:06 +0100

    Change model response to a dict

--- abaqus_parse/save_model_response.py
+++ abaqus_parse/save_model_response.py
@@ -3,19 +3,17 @@ import numpy as np
 def save_model_response(fname):
     
     data = np.loadtxt(fname, comments='%')
-    
-    time = data[:, 0]
-    S_mid_mises = data[:, 1]
-    LE_mid_mises = data[:, 2]
-    LE_mid_11 = data[:, 3]
-    LE_mid_22 = data[:, 4]
-    S_corner_mises = data[:, 5]
-    LE_corner_mises = data[:, 6]
-    LE_corner_11 = data[:, 7]
-    LE_corner_22 = data[:, 8]
 
-    model_response = [time, S_mid_mises, LE_mid_mises, LE_mid_11, 
-                      LE_mid_22, S_corner_mises, LE_corner_mises,
-                      LE_corner_11, LE_corner_22]
+    model_response = {
+        'time': data[:, 0],
+        'S_mid_mises': data[:, 1],
+        'LE_mid_mises': data[:, 2],
+        'LE_mid_11': data[:, 3],
+        'LE_mid_22': data[:, 4],
+        'S_corner_mises': data[:, 5],
+        'LE_corner_mises': data[:, 6],
+        'LE_corner_11': data[:, 7],
+        'LE_corner_22': data[:, 8],
+    }
 
     return model_response
