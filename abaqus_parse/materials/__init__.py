08e27810a96f2d19bf99ec3cd0442e29593b6500
APlowman <adam.jp@live.co.uk>
2021-04-29 10:29:17 +0100

    Formatting

--- abaqus_parse/materials/__init__.py
+++ abaqus_parse/materials/__init__.py
@@ -4,10 +4,11 @@ MATERIAL_MODEL_FUNCS = {
     'euromat_A': euromat_A.get_euromat_A_material_definition,
 }
 
+
 def generate_material_models(materials_list):
-    
+
     material_models = {}
-    
+
     for material in materials_list:
         mat_mod_i = {}
 
@@ -15,13 +16,12 @@ def generate_material_models(materials_list):
             func = MATERIAL_MODEL_FUNCS[material['func_name']]
             func_kwargs = {key: value for key, value in material.items()
                            if key not in ['name', 'func_name']}
-            mat_mod_i = func(**func_kwargs) 
+            mat_mod_i = func(**func_kwargs)
 
         else:
             if 'elastic' in material:
                 mat_mod_i['Elastic'] = material['elastic']
 
         material_models[material['name']] = mat_mod_i
-
 
     return material_models
