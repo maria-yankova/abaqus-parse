1c4b8a2ff365eaf8e45b345fb832c7a8b90f03ec
maria-yankova <maria.yankova@manchester.ac.uk>
2021-07-08 15:31:06 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     description="Abaqus input and output file readers and writers.",
     install_requires=[
         'numpy',
-        'fracture_fea_laf',
+        'fracture_fea_laf==0.1.1',
         'plotly',
     ],
     license="MIT license",
